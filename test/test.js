4f45d9b195b91a0959cee131346353e728357285
kgryte <kgryte@gmail.com>
2015-05-25 05:02:31 -0700

    [TESTS] documentation.

--- test/test.js
+++ test/test.js
@@ -24,7 +24,7 @@ describe( 'compute-incrmstdev', function tests() {
 		expect( incrmstdev ).to.be.a( 'function' );
 	});
 
-	it( 'should throw an error if not provided an integer', function test() {
+	it( 'should throw an error if not provided a positive integer', function test() {
 		var values = [
 			'5',
 			-5,
